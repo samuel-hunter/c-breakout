37bef02973b3d0a5eb980c8b08d679603bbc0028
Samuel Hunter <samuelhunter1024@gmail.com>
2018-03-10 12:49:33 -0800

    Hitting the ball can vary in angle

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ PREFIX = ${HOME}/.local
 
 CC = clang
 
-CFLAGS = -g -std=c99 $(shell sdl2-config --cflags) -Wpedantic -Os
-LDFLAGS = $(shell sdl2-config --libs) -lSDL2_ttf
+CFLAGS = -g -std=c99 -D_DEFAULT_SOURCE $(shell sdl2-config --cflags) -Wpedantic -Os
+LDFLAGS = $(shell sdl2-config --libs) -lSDL2_ttf -lm
