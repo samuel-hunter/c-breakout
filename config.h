b6103acf41011e32adb35929e1abbc59cf007c16
Samuel Hunter <samuelhunter1024@gmail.com>
2018-03-10 13:25:08 -0800

    Tweak bricks

--- config.h
+++ config.h
@@ -8,11 +8,12 @@
 #define GAME_WIDTH  800
 #define GAME_HEIGHT 600
 
-#define NUM_BRICKS   10
-#define BRICK_WIDTH  69
-#define BRICK_HEIGHT 20
-#define BRICK_WGAP   10
-#define BRICK_HGAP    5
+#define NUM_BRICKS      10
+#define BRICK_WIDTH     69
+#define BRICK_HEIGHT    20
+#define BRICK_WGAP      10
+#define BRICK_HGAP       5
+#define BRICK_Y_OFFSET  ((BRICK_HEIGHT + BRICK_HGAP)*2)
 
 #define PADDLE_WIDTH 100
 #define PADDLE_HEIGHT 20
@@ -20,6 +21,7 @@
 
 #define BALL_RADIUS       10
 #define BALL_SPEED_START 300
+#define BALL_ANGLE_START (M_PI * 1.6667)
 
 #define BORDER_SIZE 5
 
@@ -27,12 +29,13 @@ static const Color paddle_color = { .r = 0x22, .g = 0x44, .b = 0xbb };
 static const Color ball_color   = { .r = 0xff, .g = 0xff, .b = 0xff };
 
 static const Layer layers[] = {
-	{  800, { .r = 0xff, .g = 0x00, .b = 0x00 } },
-	{  800, { .r = 0xff, .g = 0x00, .b = 0x00 } },
-	{  700, { .r = 0xff, .g = 0xff, .b = 0x00 } },
-	{  500, { .r = 0x00, .g = 0xff, .b = 0x00 } },
-	{  500, { .r = 0x00, .g = 0xff, .b = 0x00 } },
-	{  400, { .r = 0x00, .g = 0xff, .b = 0xff } },
+	{  500, { .r = 0xff, .g = 0x00, .b = 0xff } },
+	{  475, { .r = 0xff, .g = 0x00, .b = 0x00 } },
+	{  475, { .r = 0xff, .g = 0x00, .b = 0x00 } },
+	{  450, { .r = 0xff, .g = 0xff, .b = 0x00 } },
+	{  400, { .r = 0x00, .g = 0xff, .b = 0x00 } },
+	{  400, { .r = 0x00, .g = 0xff, .b = 0x00 } },
+	{  350, { .r = 0x00, .g = 0xff, .b = 0xff } },
 	{  300, { .r = 0x00, .g = 0x00, .b = 0xff } },
 	{  300, { .r = 0x00, .g = 0x00, .b = 0xff } }
 };
