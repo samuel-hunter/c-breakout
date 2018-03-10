1daf31d4dab50a6ee1c1cfbee812903101a0cde3
Samuel Hunter <samuelhunter1024@gmail.com>
2018-03-09 21:46:20 -0800

    Tweaks, config

--- config.h
+++ config.h
@@ -1,6 +1,10 @@
 
 #define FPS 60 /* maximum fps */
 
+#define GAME_NAME "Breakout"
+
+#define BGCOLOR 0, 0, 0
+
 #define GAME_WIDTH  800
 #define GAME_HEIGHT 600
 
@@ -13,9 +17,8 @@
 
 Layer layers[] = {
 	{ 10, { .r = 0xff, .g = 0x00, .b = 0x00 } },
-	{  8, { .r = 0xff, .g = 0x00, .b = 0x00 } },
+	{  8, { .r = 0xff, .g = 0xff, .b = 0x00 } },
 	{  5, { .r = 0x00, .g = 0xff, .b = 0x00 } },
-	{  5, { .r = 0x00, .g = 0xff, .b = 0x00 } },
-	{  3, { .r = 0x00, .g = 0x00, .b = 0xff } },
+	{  3, { .r = 0x00, .g = 0xff, .b = 0xff } },
 	{  3, { .r = 0x00, .g = 0x00, .b = 0xff } }
 };
