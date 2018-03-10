d0bca3ea99d50bb4c2785bf1f7978a6fb7812322
Samuel Hunter <samuelhunter1024@gmail.com>
2018-03-09 21:38:23 -0800

    Tweak; move defines to config.h

--- config.h
+++ config.h
@@ -1,9 +1,21 @@
 
+#define FPS 60 /* maximum fps */
+
+#define GAME_WIDTH  800
+#define GAME_HEIGHT 600
+
+#define NUM_BRICKS   10
+#define BRICK_WIDTH  69
+#define BRICK_HEIGHT 20
+#define BRICK_WGAP   10
+#define BRICK_HGAP    5
+
+
 Layer layers[] = {
 	{ 10, { .r = 0xff, .g = 0x00, .b = 0x00 } },
-	{ 8,  { .r = 0xff, .g = 0x00, .b = 0x00 } },
-	{ 5,  { .r = 0x00, .g = 0xff, .b = 0x00 } },
-	{ 5,  { .r = 0x00, .g = 0xff, .b = 0x00 } },
-	{ 3,  { .r = 0x00, .g = 0x00, .b = 0xff } },
-	{ 3,  { .r = 0x00, .g = 0x00, .b = 0xff } }
+	{  8, { .r = 0xff, .g = 0x00, .b = 0x00 } },
+	{  5, { .r = 0x00, .g = 0xff, .b = 0x00 } },
+	{  5, { .r = 0x00, .g = 0xff, .b = 0x00 } },
+	{  3, { .r = 0x00, .g = 0x00, .b = 0xff } },
+	{  3, { .r = 0x00, .g = 0x00, .b = 0xff } }
 };
